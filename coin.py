2c2a2518dc5e78c478efd6b5523c042de7ad79ab
TechDomani <natalie.dillon@gmail.com>
2021-10-03 22:56:44 +0100

    Set a maximum and minimum amount

--- coin.py
+++ coin.py
@@ -1,9 +1,12 @@
 from function import minimum_coins_required
 
-def obtain_integer_input():
+def obtain_integer_input(maxValue=50000, minValue=1):
     while True:
         try:
-            amount = int(input("Amount in pence "))
+            amount = int(input('Please enter an amount in pence '))
+            if (amount < minValue or amount > maxValue):
+                print('You must enter a value between', minValue, 'and', maxValue)
+                continue
             return amount
         except ValueError as e:
             print('Please enter a whole number of pence')
@@ -11,4 +14,4 @@ def obtain_integer_input():
       
 amount = obtain_integer_input()
 numberOfCoins = minimum_coins_required(amount)
-print("The minimum number of coins required is ", numberOfCoins)
+print('The minimum number of coins required is', numberOfCoins)
