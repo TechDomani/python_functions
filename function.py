703ca455fe8072d5281dd073cc6e096e3d4f1d94
TechDomani <natalie.dillon@gmail.com>
2021-10-03 23:21:34 +0100

    Simplify function to use loop rather than recursion

--- function.py
+++ function.py
@@ -1,12 +1,13 @@
-def minimum_coins_required(amountInPence, limitIndex=0, coinCount=0):
+def minimum_coins_required(amountInPence):
+    coinCount = 0
     amountInPence = int(amountInPence)
     coins = [200, 100, 50, 20, 10, 5, 2, 1]
     if (amountInPence < 1):
         return coinCount
-    if (amountInPence < coins[limitIndex]):
-        limitIndex += 1
-        return minimum_coins_required(amountInPence, limitIndex, coinCount)
-    amountInPence = amountInPence - coins[limitIndex]
-    coinCount += 1
-    return minimum_coins_required(amountInPence, limitIndex, coinCount)
+    for coin in coins:
+        while amountInPence >= coin:
+            amountInPence = amountInPence - coin
+            coinCount += 1
+    return coinCount
+
 
