554b4ca22cd183541ba8ce9d4650c914ba6bdd43
TechDomani <natalie.dillon@gmail.com>
2021-10-21 11:42:56 +0100

    Allow user to choose character set

--- character.py
+++ character.py
@@ -2,7 +2,7 @@
 
 import sys
 
-print('The character encoding is', sys.getdefaultencoding())
+print('The default character encoding is', sys.getdefaultencoding())
 
 # 0b is a prefix to denote the number is in binary
 # utf-8 is unicode but variable length
@@ -30,14 +30,14 @@ def get_utf_array(val):
 
 
 val = input('What would you like to convert to binary?')
-print('The character encoding is:', sys.getdefaultencoding())
-
-# ascii encoding
-ascii_char, ascii_val = get_ascii_array(val)
-print('The ascii encoding is:', ascii_char)
-print('The ascii encoding in decimals is:', ascii_val)
-
-# unicode encoding
-utf_char, utf_val = get_utf_array(val)
-print('The utf encoding is:', utf_char)
-print('The utf encoding in decimals is:', utf_val)
+ascii = input("Would you like to use ascii encoding Y/N?")
+if (ascii.lower() == 'y'):
+  #ascii encoding
+  ascii_char, ascii_val = get_ascii_array(val)
+  print('The ascii encoding is:', ascii_char)
+  print('The ascii encoding in decimals is:', ascii_val)
+else:
+  # unicode encoding
+  utf_char, utf_val = get_utf_array(val)
+  print('The utf encoding is:', utf_char)
+  print('The utf encoding in decimals is:', utf_val)
